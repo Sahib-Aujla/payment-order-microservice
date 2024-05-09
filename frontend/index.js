1cc4491297fe05dd5409b62247f1889b73a6a332
Sahib.Aujla <sahibpreet568@gmail.com>
2024-05-09 14:30:54 -0400

    fixed folder structure

--- frontend/index.js
+++ frontend/index.js
@@ -1,98 +1,94 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchOrders();
-    fetchPayments();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchOrders();
+  fetchPayments();
 });
 
-const URL_API="http://localhost:8080/order"
+const URL_API = "http://localhost:8080/order";
 
 function fetchOrders() {
-    fetch(URL_API)
-        .then(response => response.json())
-        .then(data => {
-            
-            const table = document.getElementById('ordersTable').getElementsByTagName('tbody')[0];
-            table.innerHTML = ""; 
-            data.forEach(order => {
-                let row = table.insertRow();
-                row.insertCell(0).innerText = order.id;
-                row.insertCell(1).innerText = order.customerName;
-                row.insertCell(2).innerText = order.productName;
-                row.insertCell(3).innerText = new Date(order.orderDate).toLocaleString();
-                row.insertCell(4).innerText = order.paymentStatus;
-                row.insertCell(5).innerText = order?.totalPrice;
-                let actionsCell = row.insertCell(6);
-                actionsCell.appendChild(createButton('Delete', () => deleteOrder(order.id)));
-                actionsCell.appendChild(createButton('Update', () => updateOrderForm(order)));
-            });
-        });
+  fetch(URL_API)
+    .then((response) => response.json())
+    .then((data) => {
+      const table = document
+        .getElementById("ordersTable")
+        .getElementsByTagName("tbody")[0];
+      table.innerHTML = "";
+      data.forEach((order) => {
+        let row = table.insertRow();
+        row.insertCell(0).innerText = order.id;
+        row.insertCell(1).innerText = order.customerName;
+        row.insertCell(2).innerText = order.productName;
+        row.insertCell(3).innerText = new Date(
+          order.orderDate
+        ).toLocaleString();
+        row.insertCell(4).innerText = order.paymentStatus;
+        row.insertCell(5).innerText = order?.totalPrice;
+        let actionsCell = row.insertCell(6);
+        actionsCell.appendChild(
+          createButton("Delete", () => deleteOrder(order.id))
+        );
+      });
+    });
 }
 
-const URL_PAY="http://localhost:8081/payment/all"
-function fetchPayments(){
-    fetch(URL_PAY)
-        .then(response => response.json())
-        .then(data => {
-           
-            const table = document.getElementById('paymentsTable').getElementsByTagName('tbody')[0];
-            table.innerHTML = ""; 
-            data.forEach(payment => {
-                let row = table.insertRow();
-                row.insertCell(0).innerText = payment?.paymentId;
-                row.insertCell(1).innerText = payment?.orderId;
-                row.insertCell(2).innerText = payment?.paymentAmount;
-                row.insertCell(3).innerText = new Date(payment?.paymentDate).toLocaleString();
-                row.insertCell(4).innerText = payment?.paymentStatus;
-            });
-        });
+const URL_PAY = "http://localhost:8081/payment/all";
+function fetchPayments() {
+  fetch(URL_PAY)
+    .then((response) => response.json())
+    .then((data) => {
+      const table = document
+        .getElementById("paymentsTable")
+        .getElementsByTagName("tbody")[0];
+      table.innerHTML = "";
+      data.forEach((payment) => {
+        let row = table.insertRow();
+        row.insertCell(0).innerText = payment?.paymentId;
+        row.insertCell(1).innerText = payment?.orderId;
+        row.insertCell(2).innerText = payment?.paymentAmount;
+        row.insertCell(3).innerText = new Date(
+          payment?.paymentDate
+        ).toLocaleString();
+        row.insertCell(4).innerText = payment?.paymentStatus;
+      });
+    });
 }
 
 function createButton(text, func) {
-    let btn = document.createElement('button');
-    btn.textContent = text;
-    btn.onclick = func;
-    return btn;
+  let btn = document.createElement("button");
+  btn.textContent = text;
+  btn.onclick = func;
+  return btn;
 }
 
 function submitOrder() {
-    console.log('here');
-    let url = URL_API+'/placeOrder';
-    let method = 'POST';
-    const orderId = document.getElementById('orderId').value;
-    if (orderId) {
-        console.log("here 2")
-        url += `/${orderId}`;
-        method = 'PUT';
-    }
-    const order = {
-        customerName: document.getElementById('customerName').value,
-        productName: document.getElementById('productName').value,
-        totalPrice: parseFloat(document.getElementById('totalPrice').value),
-        quantity:parseInt(document.getElementById('quantity').value)
-    };
-    console.log({order});
-    fetch(url, {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(order),
-    }).then(response => response.json())
-      .then(() => {
-        fetchOrders(); 
-        document.getElementById('orderForm').reset();
-        document.getElementById('orderId').value = '';
-      });
+  console.log("here");
+  let url = URL_API + "/placeOrder";
+  let method = "POST";
+  
+  const order = {
+    customerName: document.getElementById("customerName").value,
+    productName: document.getElementById("productName").value,
+    totalPrice: parseFloat(document.getElementById("totalPrice").value),
+    quantity: parseInt(document.getElementById("quantity").value),
+  };
+  console.log({ order });
+  fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(order),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      fetchOrders();
+      document.getElementById("orderForm").reset();
+      document.getElementById("orderId").value = "";
+    });
 }
 
 function deleteOrder(orderId) {
-
-    fetch(URL_API+`/delete/${orderId}`, { method: 'DELETE' })
-        .then(() => fetchOrders()); 
-}
-
-function updateOrderForm(order) {
-    document.getElementById('orderId').value = order.orderId;
-    document.getElementById('customerName').value = order.customerName;
-    document.getElementById('productName').value = order.productName;
-    document.getElementById('totalPrice').value = order.totalPrice;
+  fetch(URL_API + `/delete/${orderId}`, { method: "DELETE" }).then(() =>
+    fetchOrders()
+  );
 }
